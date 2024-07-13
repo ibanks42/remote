93cdb88bd51f275144d9123172dbfce0156610d2
Isaiah Banks <isaiah.banks42@gmail.com>
2024-07-12 23:01:01 -0400

    Remove unused stuff from app main page

--- remote-tauri/src/home.tsx
+++ remote-tauri/src/home.tsx
@@ -4,15 +4,12 @@ import { Card, CardContent, CardHeader, CardTitle } from './components/ui/card';
 
 export default function HomePage() {
 	async function pauseMpv() {
-		// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 		await invoke('handle_pause_cmd');
 	}
 	async function volumeUp() {
-		console.log('volume up cmd');
 		await invoke('handle_volume_up_cmd');
 	}
 	async function volumeDown() {
-		alert('volume down cmd');
 		await invoke('handle_volume_down_cmd');
 	}
 
